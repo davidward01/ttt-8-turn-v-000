d877517bf79596f7aeba43a5c0213a762c214e4c
David Ward <davidward.info@gmail.com>
2021-03-11 21:21:42 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,8 @@ def valid_move?(board, position)
     if !position_taken?(board, position.to_i-1)
       true
     end
-  end
+end
+
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
 end
