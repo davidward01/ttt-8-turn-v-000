2bd0de144088b70f05d030b933075a3db47806ca
David Ward <davidward.info@gmail.com>
2021-03-11 20:50:30 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,30 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(number)
+  number.to_i - 1
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  else
+    true
+  end
+end
+
+def valid_move?(board, index)
+  if index.between?(1,9) && !position_taken?(board, index)
+    true
+  else
+    false
+  end
+end
+
+def move
+end
