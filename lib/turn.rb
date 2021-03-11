c335256d0ea2b0dde7efbe4a67090f24e00540ad
David Ward <davidward.info@gmail.com>
2021-03-11 21:12:21 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,40 +6,31 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def valid_move?(board, index)
-  if index.to_i.between?(1,9) && !position_taken?(board, index.to_i-1)
-    true
+def valid_move?(board, position)
+  if position.to_i.between?(1,9)
+    if !position_taken?(board, position.to_i-1)
+      true
+    end
   end
-end
 
+  # position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+end
 
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.chomp
+  input = gets.strip
   if valid_move?(board, input)
     move(board, input)
   else
-    turn
+    turn(board)
   end
   display_board(board)
 end
 
-
-def position_taken?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil
-    false
-  else
-    true
-  end
+def position_taken?(board, location)
+  board[location] != " "
 end
 
-def move(board, index, player="X")
-  board[index.to_i-1] = player
+def move(board, location, current_player = "X")
+  board[location.to_i-1] = current_player
 end
-
-
-
-
-#def move(board, index)
-
-#end
